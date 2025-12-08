d81f28e0ddb02f97f3a66e204e681c71d37b5765
Anirban chattaraj <anirbanchattaraj@Anirbans-MacBook-Air.local>
2025-12-08 20:09:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const faders = document.querySelectorAll('.fade-in');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+faders.forEach(fader => observer.observe(fader));
